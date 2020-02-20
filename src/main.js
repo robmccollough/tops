305cd825eb77cbef6e42d2860d4f2cdbe7428394
robmccollough <admin@robmccollough.com>
2020-02-20 11:31:57 -0500

    rdrct uri

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Struct from "./Struct.svelte";
 
 const struct = new Struct({
-  target: document.body,
-  props: {
-    client_id: "3e5b56661b4b42d1bbac49ebc178cfdc",
-    redirect_uri: "http://localhost:5000"
-  }
+	target: document.body,
+	props: {
+		client_id: "3e5b56661b4b42d1bbac49ebc178cfdc",
+		redirect_uri: "https://robmccollough.dev/tops"
+	}
 });
 
 export default struct;
