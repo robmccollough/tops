3a0cec9414bc33b6e569613616f5316e1fceb90d
robmccollough <admin@robmccollough.com>
2021-01-04 12:53:13 -0500

    creds update

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const struct = new Struct({
 	target: document.body,
 	props: {
 		client_id: "76fcd2e72e5c44aabcf9530cdb899522",
-		redirect_uri: "https://robmccollough.dev/tops",
+		redirect_uri: "http://localhost:5000",
 	},
 });
 
